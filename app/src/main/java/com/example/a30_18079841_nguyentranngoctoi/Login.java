9765eb6dbc884fa686f7b7f51b120601c9aee26c
Nguyễn Trần Ngọc Tới <nguyentoi53@gmail.com>
2021-06-01 10:28:49 +0700

    xong gui login

---
+++ app/src/main/java/com/example/a30_18079841_nguyentranngoctoi/Login.java
@@ -0,0 +1,13 @@
+package com.example.a30_18079841_nguyentranngoctoi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ app/src/main/java/com/example/a30_18079841_nguyentranngoctoi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a30_18079841_nguyentranngoctoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
